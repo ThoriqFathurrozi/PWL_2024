676fa7033ebc34da44e101ed117ced1fe9266c41
thoriq fathurrozi <thoriqfathurrozi@gmail.com>
2024-02-28 15:01:55 +0700

    feat: praktikum 1,2,3

--- routes/web.php
+++ routes/web.php
@@ -13,33 +13,140 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Praktikum 1
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
 Route::get('/', function () {
-    return view('mywelcome');
+    return 'Selamat Datang';
 });
 
-Route::get('mahasiswa', function ($id) {
-});
-Route::post('mahasiswa', function ($id) {
-});
-Route::put('mahasiswa', function ($id) {
-});
-Route::delete('mahasiswa', function ($id) {
-});
-Route::get('mahasiswa/{id}', function ($id) {
-});
-Route::put('mahasiswa/{id}', function ($id) {
-});
-Route::delete('mahasiswa/{id}', function ($id) {
+Route::get('/about', function () {
+    return 'NIM : 2241720052 <br> Nama : Thoriq Fathurrozi';
 });
 
-Route::get('hello', function () {
-    return view('hello');
+//Praktikum 2
+Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya ' . $name;
 });
 
-Route::match(['get'], '/world', function () {
-    return view('world');
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
 
-Route::any('about', function () {
-    return view('about');
+
+//Praktikum 3
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama Saya ' . $name;
 });
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+
+
+
+//MY Explore 
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('mahasiswa', function ($id) {
+// });
+
+// Route::post('mahasiswa', function ($id) {
+// });
+
+// Route::put('mahasiswa', function ($id) {
+// });
+
+// Route::delete('mahasiswa', function ($id) {
+// });
+
+// Route::get('mahasiswa/{id}', function ($id) {
+// });
+
+// Route::put('mahasiswa/{id}', function ($id) {
+// });
+
+// Route::delete('mahasiswa/{id}', function ($id) {
+// });
+
+// Route::get('hello', function () {
+//     return view('hello');
+// });
+
+// Route::match(['get'], '/world', function () {
+//     return view('world');
+// });
+
+// Route::any('about', function () {
+//     return view('about');
+// });
+
+
+
+// Route::get('/user/{name}', function ($name) {
+//     return view('user', ['name' => $name]);
+// });
+
+// Route::get('/user/{name?}', function ($name = 'John Doe') {
+//     return view('user', ['name' => $name]);
+// });
+
+// Route::match(['GET'], 'posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return view('comment', ['postId' => $postId, 'commentId' => $commentId]);
+// });
+
+
+// Route::get('/user/profile', function () {
+//     //
+// })->name('profile');
+// Route::get(
+//     '/user/profile',
+//     [UserProfileController::class, 'show']
+// )->name('profile');
+// // Generating URLs...
+// $url = route('profile');
+// // Generating Redirects...
+// return redirect()->route('profile');
+
+
+// Route::middleware(['first', 'second'])->group(function () {
+//     Route::get('/', function () {
+//     // Uses first & second middleware...
+//     });
+//     Route::get('/user/profile', function () {
+//     // Uses first & second middleware...
+//     });
+//     });
+//     Route::domain('{account}.example.com')->group(function () {
+//     Route::get('user/{id}', function ($account, $id) {
+//     //
+//     });
+//     });
+//     Route::middleware('auth')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+//     });
+
+
+// Route::prefix('admin')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+//     });
+
+// Route::redirect('/here', '/there');
+
+
+// Route::view('/welcome', 'welcome');
+// Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
